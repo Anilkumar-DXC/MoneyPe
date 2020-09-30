71ac4431bdd803b15ef2ebb1d9a975149ef439f0
Anilkumar <anil.tony33@gmail.com>
2020-09-30 11:37:01 +0530

    swagger

---
+++ master/MoneyPe/src/main/java/com/dxc/moneype/controller/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.dxc.moneype.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.common.base.Predicates;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	@Bean
+	public Docket  docket() {
+		
+		return  new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+				.apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot"))).build();
+		
+	}
+	
+private ApiInfo apiInfo() {
+		
+		return new ApiInfo("MoneyPe", "This is MoneyPe API", "v1.1", "serviceurl", "Team", "available", "na");  		
+	}
+
+
+}
